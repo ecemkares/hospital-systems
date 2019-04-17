c162f5d13e500b27d7f536dcd7175a62e6a6892e
ecemkares <48796331+ecemkares@users.noreply.github.com>
2019-04-17 13:06:38 +0300

    Add files via upload

---
+++ HospitalSystem.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospitalsystem;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author asus
+ */
+public class HospitalSystem extends JApplet {
+    
+    private static final int JFXPANEL_WIDTH_INT = 300;
+    private static final int JFXPANEL_HEIGHT_INT = 250;
+    private static JFXPanel fxContainer;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() {
+                homePage obj = new homePage();
+                obj.setVisible(true);
+            }
+        });
+    }
+   
+    
+   
+    
+}
